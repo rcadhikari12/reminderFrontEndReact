c68a41fdd38a1a27ec67d5b04c627706def8bf58
Ram Chandra Adhikari <ram_chandra_2008@yahoo.com>
2020-08-30 14:11:31 -0500

    Updates

---
+++ precache-manifest.b41854951aa05343e9971bda0e6d0e68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b378c76902ee2ffd93f0c35c9eef79",
+    "url": "/reminderFrontEndReact/index.html"
+  },
+  {
+    "revision": "da495e78abdc96bdda40",
+    "url": "/reminderFrontEndReact/static/css/main.19925d6b.chunk.css"
+  },
+  {
+    "revision": "e75b2f414d80073593fe",
+    "url": "/reminderFrontEndReact/static/js/2.ec74ec21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reminderFrontEndReact/static/js/2.ec74ec21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da495e78abdc96bdda40",
+    "url": "/reminderFrontEndReact/static/js/main.1a41bc45.chunk.js"
+  },
+  {
+    "revision": "2ae741769abc398a0d7c",
+    "url": "/reminderFrontEndReact/static/js/runtime-main.737a1c45.js"
+  }
+]);
